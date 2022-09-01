c910c8c9d06dd17d418397a97eb1d9fb77dc92e4
Alessimc <alessimc@student.geo.uio.no>
2022-09-01 12:21:30 +0200

    relativ path fiksa

--- project1/src/problem2plot.py
+++ project1/src/problem2plot.py
@@ -8,4 +8,4 @@ u = data [:,1]
 plt.plot(x,u)
 plt.xlabel("$x$")
 plt.ylabel("$u(x)$")
-plt.savefig("/Users/alessiocanclini/FYS4150/project1/figures/x_u_plot.pdf")
+plt.savefig("../figures/x_u_plot.pdf")
