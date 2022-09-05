81a30cc6153f99ff56131c1325ba7dc23598ee8d
Fslippe <fvonderlippe@gmail.com>
2022-09-05 16:33:46 +0200

    test branch

---
+++ project1/src/abs_error.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_u = np.loadtxt("x_u.txt")
+x_v = np.loadtxt("x_v.txt")
+
+plt.plot(np.log10(abs(x_u[:,1] - x_v[:,1])))
+plt.title("Absolute error")
+plt.show()
