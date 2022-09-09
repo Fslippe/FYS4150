a246e195272c611c9b666b95bd1ab4309f2a0023
Fslippe <fvonderlippe@gmail.com>
2022-09-09 13:58:23 +0200

    saved figures from timeplots

--- project1/src/general_algorithm.cpp
+++ project1/src/general_algorithm.cpp
@@ -8,8 +8,8 @@
 arma::vec general_algorithm(arma::vec v, arma::vec g, arma::vec a, arma::vec b, arma::vec c); // Declaration of u(x).
 
 int main() {
-  int n = 10000000; // Number of steps
-  int ti = 1; // number of time
+  int n = 10; // Number of steps
+  int ti = 1000; // number of time
 
   arma::vec x = arma::linspace(0, 1, n+1); //Declare and will with random uniform values.
   arma::vec g = arma::vec(n-1);
@@ -34,7 +34,7 @@ int main() {
     double duration_seconds = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
     time[i] = duration_seconds;
   }
-  time.save("general_algorithm_time10.dat");
+  time.save("data/general_algorithm_time10.dat");
 
 
   for (int i = 1; i <= n; i++)
@@ -47,7 +47,7 @@ int main() {
   arma::mat data = arma::mat(n+1, 2);
   data.col(0) = x;
   data.col(1) = v_full;
-  data.save("n10000000.dat");
+  data.save("data/n10000000.dat");
 
   return 0;
 }
--- project1/src/time.py
+++ project1/src/time.py
@@ -1,61 +1,75 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pyarma as pa
+import numpy as np
 
+#Using armadillo to import data files
 A = pa.mat()
+B = pa.mat()
+#Creating arrays to fill
 mean_g = np.zeros(6)
 std_g = np.zeros(6)
-B = pa.mat()
 mean_s = np.zeros(6)
 std_s = np.zeros(6)
+x = [1e1, 1e2, 1e3, 1e4, 1e5, 1e6] #list with number of steps for the different datafiles
 
-A.load("general_algorithm_time10.dat")
+#Loading arrays for different number of steps and assigning values to arrays
+#General algorithm:
+A.load("data/general_algorithm_time10.dat")
 mean_g[0] = pa.mean(A)[0]
 std_g[0] = pa.stddev(A)[0]
-A.load("general_algorithm_time100.dat")
+A.load("data/general_algorithm_time100.dat")
 mean_g[1] = pa.mean(A)[0]
 std_g[1] = pa.stddev(A)[0]
-A.load("general_algorithm_time1000.dat")
+A.load("data/general_algorithm_time1000.dat")
 mean_g[2] = pa.mean(A)[0]
 std_g[2] = pa.stddev(A)[0]
-A.load("general_algorithm_time10000.dat")
+A.load("data/general_algorithm_time10000.dat")
 mean_g[3] = pa.mean(A)[0]
 std_g[3] = pa.stddev(A)[0]
-A.load("general_algorithm_time100000.dat")
+A.load("data/general_algorithm_time100000.dat")
 mean_g[4] = pa.mean(A)[0]
 std_g[4] = pa.stddev(A)[0]
-A.load("general_algorithm_time1000000.dat")
+A.load("data/general_algorithm_time1000000.dat")
 mean_g[5] = pa.mean(A)[0]
 std_g[5] = pa.stddev(A)[0]
 
-
-B.load("special_algorithm_time10.dat")
+#Special algorithm:
+B.load("data/special_algorithm_time10.dat")
 mean_s[0] = pa.mean(B)[0]
 std_s[0] = pa.stddev(B)[0]
-B.load("special_algorithm_time100.dat")
+B.load("data/special_algorithm_time100.dat")
 mean_s[1] = pa.mean(B)[0]
 std_s[1] = pa.stddev(B)[0]
-B.load("special_algorithm_time1000.dat")
+B.load("data/special_algorithm_time1000.dat")
 mean_s[2] = pa.mean(B)[0]
 std_s[2] = pa.stddev(B)[0]
-B.load("special_algorithm_time10000.dat")
+B.load("data/special_algorithm_time10000.dat")
 mean_s[3] = pa.mean(B)[0]
 std_s[3] = pa.stddev(B)[0]
-B.load("special_algorithm_time100000.dat")
+B.load("data/special_algorithm_time100000.dat")
 mean_s[4] = pa.mean(B)[0]
 std_s[4] = pa.stddev(B)[0]
-B.load("special_algorithm_time1000000.dat")
+B.load("data/special_algorithm_time1000000.dat")
 mean_s[5] = pa.mean(B)[0]
 std_s[5] = pa.stddev(B)[0]
 
-x = [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
-plt.plot(x, mean_g)
-plt.plot(x, mean_s)
+#plots:
+plt.title("Mean time over 1000 runs for different number of steps")
+plt.plot(x, mean_g, label="General algorithm")
+plt.plot(x, mean_s, label="Special algorithm")
+plt.xlabel("Number of steps (n)")
+plt.ylabel("Time (s)")
 plt.xscale("log")
-
+plt.legend()
+plt.savefig("../figures/mean_time.pdf")
 plt.show()
-plt.plot(x, std_g)
-plt.plot(x, std_s)
-plt.xscale("log")
 
+plt.title("Standard deviation over 1000 runs for different number of steps")
+plt.plot(x, std_g, label="General algorithm")
+plt.plot(x, std_s, label="Special algorithm")
+plt.xscale("log")
+plt.xlabel("Number of steps (n)")
+plt.ylabel("$\sigma$ (s)")
+plt.legend()
+plt.savefig("../figures/std_time.pdf")
 plt.show()
