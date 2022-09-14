1fe518dfee751d423226c01cf7d04f4c115d6967
Alessimc <alessimc@student.geo.uio.no>
2022-09-14 22:09:11 +0200

    matrix print test

--- project2/src/create_tridiagonal.cpp
+++ project2/src/create_tridiagonal.cpp
@@ -17,3 +17,18 @@ arma::mat create_tridiagonal(int n, double a, double d, double e)
   
   return A;
 }
+
+// print to test
+int main()
+{
+int n = 30;
+arma::mat A = create_tridiagonal(n,6,1,9);
+for (int i = 0; i < n; i++)
+{
+        for (int j = 0; j < n; j++) {
+            std::cout << A(i,j) << ' ';
+        }
+        std::cout << std::endl;
+}
+  return 0;
+}
