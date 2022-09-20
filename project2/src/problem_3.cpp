83903e2c0a5b731d240956b34ae6a67a3731380d
Fslippe <fvonderlippe@gmail.com>
2022-09-20 12:27:51 +0200

    fixed problem3a

--- project2/src/problem_3.cpp
+++ project2/src/problem_3.cpp
@@ -5,42 +5,46 @@
 #include <fstream>
 #include <iomanip>
 
-double max_offdiag_symmetric(const arma::mat A, int k, int l);
+double max_offdiag_symmetric(const arma::mat& A, int& k, int &l);
 
 // test func using A given in prob 3 b).
 int main()
 {
- int k = 0; int l = 1;
- arma::mat A = {{1, 0, 0, 0.5},
+  int k = 0; int l = 1;
+  arma::mat A = {{1, 0, 0, 0.5},
                 {0, 1, -0.7, 0},
                 {0, -0.7, 1, 0},
                 {0.5, 0, 0, 1}};
 
- int max = max_offdiag_symmetric(A, k, l);
+  double max = max_offdiag_symmetric(A, k, l);
 
   A.print();
- std::cout << arma::endl;
- std::cout << "maxval: "<< max << arma::endl;
-    return 0;
+  std::cout << arma::endl;
+  std::cout << "maxval: "<< max << arma::endl;
+  return 0;
 }
 
 // Finding the max off-diagonal element of a matrix
-double max_offdiag_symmetric(const arma::mat A, int k, int l)
+double max_offdiag_symmetric(const arma::mat& A, int& k, int &l)
 {
- int N = A.n_rows;
- assert(N > 1); 
- assert(A.is_square());
+  int N = A.n_rows;
+  assert(N > 1);
+  assert(A.is_square());
 
- double maxval = A(k,l); //(i,j) (row, col)
- int col_n = 1;
+  double maxval = std::abs(A(k,l)); //(i,j) (row, col)
+  std::cout << maxval << "\n";
 
- for (int i = 0; i <= N-2; ++i){
-  for (int j = col_n; j <=N-1; ++j){
-    std::cout << A(i,j) << arma::endl;
-    if(std::abs(A(i,j)) > std::abs(maxval)){
+  int col_n = 1;
+
+  for (int i = 1; i <= N-1; ++i) //Loops over rows
+  {
+    for (int j = 0; j < i; ++j) //loops over columns until the subdiagonal
+    {
+      if(std::abs(A(i,j)) > maxval)
+      {
         k = i; l = j;
-    }
-    col_n += 1;
+        maxval = std::abs(A(i,j));
+      }
    }
   }
   return maxval;
