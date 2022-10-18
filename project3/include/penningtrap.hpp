209b77014773d48634eb8211f3d33855c60e051d
Fslippe <fvonderlippe@gmail.com>
2022-10-18 13:55:02 +0200

    added all forces in penningtrap

--- project3/include/penningtrap.hpp
+++ project3/include/penningtrap.hpp
@@ -1,6 +1,6 @@
 // PenningTrap class
 
-#ifndef __penningtrap_hpp__  
+#ifndef __penningtrap_hpp__
 #define __tenningtrap_hpp__
 
 #include <string>
@@ -13,24 +13,38 @@ class PenningTrap
     double B_0; // magnetic field strength
     double V_0; // applied potetial
     double d; // characteristic dimension
+    B_0 = B_0_in;
     std::vector<Particle> particle_objects; // conntains Particle objects in the Penning trap
 
     // Constructor
-    PenningTrap(double B_0_in, double V_0_in, double d_in, std::vector<Particle> particle_objects_in);
+    PenningTrap(double B0_in, double V0_in, double d_in, std::vector<Particle> particle_objects);
 
-
-    //Method that adds a particle to the Penning trap
+    // Add a particle to the trap
     void add_particle(Particle p_in);
 
-    // Method that returns the ecternal electric field
-    arma::vec ext_el_field();
+    // External electric field at point r=(x,y,z)
+    arma::vec external_E_field(arma::vec r);
 
-    // Method that returns the ecternal magnetic field
-    arma::vec ext_mag_field();
+    // External magnetic field at point r=(x,y,z)
+    arma::vec external_B_field(arma::vec r);
 
-    // Method that returns the force due to the interaction among particles
-    arma::vec p_interaction_force();
+    // Force on particle_i from particle_j
+    arma::vec force_particle(int i, int j);
 
+    // The total force on particle_i from the external fields
+    arma::vec total_force_external(int i);
+
+    // The total force on particle_i from the other particles
+    arma::vec total_force_particles(int i);
+
+    // The total force on particle_i from both external fields and other particles
+    arma::vec total_force(int i);
+
+    // Evolve the system one time step (dt) using Runge-Kutta 4th order
+    void evolve_RK4(double dt);
+
+    // Evolve the system one time step (dt) using Forward Euler
+    void evolve_forward_Euler(double dt);
 };
 
 #endif
--- project3/src/penningtrap.cpp
+++ project3/src/penningtrap.cpp
@@ -4,60 +4,86 @@
 //#include "particle.hpp"
 
 // Constructor
-PenningTrap::PenningTrap(double B_0_in, double V_0_in, double d_in, std::vector<Particle> particle_objects_in)
+// Constructor
+PenningTrap(double B0_in, double V0_in, double d_in, std::vector<Particle> particle_objects_in);
 {
-  B_0 = B_0_in;
-  V_0 = V_0_in;
-  d = d_in;
-  particle_objects = particle_objects_in;
+  double B_0 = B_0_in;
+  double V_0 = V_0_in;
+  double d = d_in;
+  Particle particle_objects = particle_objects_in;
 }
-
-//Method that adds a particle to the Penning trap
-void PenningTrap::add_particle(Particle p_in)
+// Add a particle to the trap
+void add_particle(Particle p_in);
 {
   particle_objects.push_back(p_in);
 }
-
-//Method that adds several particles in a vector to the Penning trap
-void PenningTrap::add_particles(std::vector<Particle> particles_in)
-{
-  for (p_in : particles_in)
-  {
-    particle_objects.push_back(p_in);
-  }
-}
-
-// Method that returns the external electric field
-arma::vec PenningTrap::ext_el_field(double x, double y, double z, double V_0, double d)
+// External electric field at point r=(x,y,z)
+arma::vec external_E_field(arma::vec r);
 {
   //Analytic gradient:
-  double E_x = -x*V_0/(d*d);
-  double E_y = -y*V_0/(d*d);
-  double E_z = 2*z*V_0/(d*d);
+  double E_x = -r[0]*V_0/(d*d);
+  double E_y = -r[1]*V_0/(d*d);
+  double E_z = 2*r[2]*V_0/(d*d);
 
   return arma::vec(-E_x, -E_y, -E_z);
 }
 
-// Method that returns the external magnetic field
-arma::vec PenningTrap::ext_mag_field()
+// External magnetic field at point r=(x,y,z)
+arma::vec external_B_field(arma::vec r);
 {
   return arma::vec(0, 0, B_0)
 }
 
-// Method that returns the force due to the interaction among particles
-arma::vec PenningTrap::p_interaction_force(arma::vec r, arma::vec q)
+// Force on particle_i from particle_j
+arma::vec force_particle(int i, int j);
 {
-  int n = r.size()
-  for (size_t i = 0; i < n; i++)
+  Particle p_i = particle_objects[i];
+  Particle p_j = particle_objects[j];
+
+  r_diff = p_i.position - p_j.position;
+  E = k_e * p_j.charge * r_diff / std::pow(std::sqrt(r_diff[0]*r_diff[0] + r_diff[0]*r_diff[0] + r_diff[0]*r_diff[0]), 3);
+
+  return p_i.charge * E;
+}
+
+// The total force on particle_i from the external fields
+arma::vec total_force_external(int i);
+{
+  Particle p = particle_objects[i];
+  arma::vec B = external_B_field(p.position);
+  arma::vec E = external_E_field(p.position);
+  arma::vec F_ext = p.charge * E + p.charge*arma::cross(p.velocity, B);
+
+  return F_ext;
+}
+
+// The total force on particle_i from the other particles
+arma::vec total_force_particles(int i);
+{
+  int n = (particle_objects.charge).size;
+  arma::vec F_p = arma::vec(3);
+
+  for (int j = 0; j < n; j++)
   {
-    for (int j = 0; j < n; j++)
-    {
-      if (i != j)
-      {
-        E[i] = E[i] + k_e*q[j]* (r[i] - r[j]) / std::pow(arma::abs(r[i] - r[j]), 3)
-      }
-    }
+    F_p = F_p + force_particle(i, j);
   }
 
-  arma::vec F =
+  return F_p;
+}
+
+// The total force on particle_i from both external fields and other particles
+arma::vec total_force(int i);
+{
+  arma::vec F_tot = total_force_particles(i) + total_force_external(i);
+  return F_tot
+}
+// Evolve the system one time step (dt) using Runge-Kutta 4th order
+void evolve_RK4(double dt);
+{
+
+}
+// Evolve the system one time step (dt) using Forward Euler
+void evolve_forward_Euler(double dt);
+{
+
 }
