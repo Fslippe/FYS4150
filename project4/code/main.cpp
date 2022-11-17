6d9c72aa03ec5e6628e71f9b5685f21518107a31
Fslippe <fvonderlippe@gmail.com>
2022-11-17 13:45:52 +0100

    updated font size for plots N=10000

--- project4/code/main.cpp
+++ project4/code/main.cpp
@@ -5,11 +5,11 @@
 int main()
 {
     int seed = 1032;
-    double T = 2.4;
-    int cycles = 20;
+    double T = 1;
+    int cycles = 100;
     int lattice_dim = 2;
     IsingModel IM = IsingModel(lattice_dim);
-    IM.init_lattice(T, seed, false);
+    IM.init_lattice(T, seed, true);
     IM.MC_sample(cycles);
     return 0;
 }
