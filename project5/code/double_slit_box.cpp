b854eb2cbc0ca0e5348932d120e9c4b0e01a673e
Alessimc <alessimc@student.geo.uio.no>
2022-12-03 15:25:06 +0100

    member variables added to constructor. first  version of init_wave fucntion written NB! u is not normalized yet + no idea if anything works :)

--- project5/code/double_slit_box.cpp
+++ project5/code/double_slit_box.cpp
@@ -1,9 +1,35 @@
 #include "double_slit_box.hpp"
 
 // Constructor
-DoubleSlitBox::DoubleSlitBox()
+DoubleSlitBox::DoubleSlitBox(double h_in, double dt_in, double T_in, double xc_in, double sigmax_in, double px_in, double yc_in, double sigmay_in, double py_in, int v0_in)
 {
-    
+    //Member variables
+    h = h_in;
+    dt = dt_in;
+    T = T_in;
+    xc = xc_in;
+    sigmax = sigmax_in;
+    px = px_in;
+    yc = yc_in;
+    sigmay = sigmay_in;
+    py = py_in;
+    v0 = v0_in;
+
+    //Sizes
+    M = 1/h; // total matrix size MxM
+    n = (M-2); // inner box nxn
+    N = n*n; // CN matix size NxN, vector size N
+
+    //Double slix box matrices (total matrix size)
+    //arma::cx_mat U(M,M);
+    arma::cx_mat V(M,M);
+
+    //Crank Nicholson matrices and vectors (internal matrix size)
+    arma::cx_vec b(N);
+    arma::cx_vec u(N);
+    arma::sp_cx_mat A(N,N);
+    arma::sp_cx_mat B(N,N);
+
 }
 
 // Tranlates a pair of indices (i,j) from an u^n (M-2)x(M-2) matrix to a corresponding single index k in a u^n vector.
@@ -13,12 +39,13 @@ int DoubleSlitBox::translate_indices(int i, int j)
 }
 
 // Fills mnatrices A and B for CN scheme 
+// NB! needs to return A B or A B are created in constructor
 void DoubleSlitBox::fill_A_B( int M, double h, double dt, arma::cx_mat V)
 {
-    int n = (M-2);
-    int N = n*n; // matix size NxN
+    // int n = (M-2);
+    // int N = n*n; // matix size NxN
 
-//-------------------------------------------- VECTOR FILLER
+    //-------------------------------------------- VECTOR FILLER
     // complex values
     std::complex<double> r(0, (dt / (2.*h*h)));
     std::complex<double> a_term = 1. + 4.*r;
@@ -34,15 +61,15 @@ void DoubleSlitBox::fill_A_B( int M, double h, double dt, arma::cx_mat V)
     {
          for (int j = 1; i <= n; i++ )
          {
-            a( translate_indices( i, j) ) = a_term + ab_term * V(i,j);
-            b( translate_indices( i, j) ) = b_term - ab_term * V(i,j);
+            a( translate_indices( i, j) ) = a_term + ab_term * V( i, j);
+            b( translate_indices( i, j) ) = b_term - ab_term * V( i, j);
          }
     }
 
-//--------------------------------------------- MATRIX FILLER
-    // Create A and B matrix
-    arma::sp_cx_mat A(N,N); // change to cx_mat for more readable print()
-    arma::sp_cx_mat B(N,N);
+    //--------------------------------------------- MATRIX FILLER
+    // // Create A and B matrix
+    // arma::sp_cx_mat A(N,N); // change to cx_mat for more readable print()
+    // arma::sp_cx_mat B(N,N);
 
     //fill diagonal and +-(M-2) super- and subdiagonal
     A.diag() = a;
@@ -68,25 +95,46 @@ void DoubleSlitBox::fill_A_B( int M, double h, double dt, arma::cx_mat V)
             B.diag(1)(i) = r;
             B.diag(-1)(i) = r;
         }
-
     }
-
 }
 
 // Evolves the system one time step (dt) using the Crank-Nicholson scheme
 void DoubleSlitBox::evolve_CN(double dt)
 {
     b = B * u;
-    u = arma::spsolve(A,b);
+    u = arma::spsolve(A,b); // spslover is well suited since A is a sparse matrix
 }
 
-// Sets up initial state based on an unnormalised Gaussian wave packet epression
-void DoubleSlitBox::init_box()
+// Sets up initial state vector u0 based on an unnormalised Gaussian wave packet epression
+void DoubleSlitBox::init_wave()
 {
+    // real and imaginary components
+    double wp_Re;
+    std::complex<double> wp_Im;
+    // wave packet value at point (x,y)
+    std::complex<double> wp_val;
+    double x;
+    double y;
 
+    // loop over all (x,y) points
+    for (int i = 1; i <= n; i++ )
+    {
+         for (int j = 1; i <= n; i++ )
+         {
+            x = i*h;
+            y = i*h;
+            
+            // not sure if the below compelx nr esp works
+            wp_Re = - (std::pow(x-xc,2)/ (2*std::pow(sigmax,2))) - (std::pow(y-yc,2)/ (2*std::pow(sigmay,2)));
+            wp_Im = (0,px*(x-xc) + py*(y-yc));
+            wp_val = std::exp(wp_Re + wp_Im);
+            u( translate_indices( i, j) ) = wp_val;
+         }
+    }
+   
 }
 
-// initializes the potential V
+// initializes the potential V matrix
 void DoubleSlitBox::init_V()
 {
 
--- project5/code/double_slit_box.hpp
+++ project5/code/double_slit_box.hpp
@@ -9,13 +9,38 @@
 
 class DoubleSlitBox
 {
-    private:
-    int M;
-
-
     public:
+
+    //Member variables
+    double h;
+    double dt;
+    double T;
+    double xc;
+    double sigmax;
+    double px;
+    double yc;
+    double sigmay;
+    double py;
+    int v0;
+
+    //Sizes
+    int M; // total matrix size MxM
+    int n; // inner box nxn
+    int N; // CN matix size NxN, , vector size N
+
+    //Double slix box matrices (total matrix size)
+    //arma::cx_mat U;
+    arma::cx_mat V;
+
+    //Crank Nicholson matrices and vectors (internal matrix size)
+    arma::cx_vec b;
+    arma::cx_vec u;
+    arma::sp_cx_mat A;
+    arma::sp_cx_mat B;
+
+
     // Consturctor
-    DoubleSlitBox();
+    DoubleSlitBox(double h_in, double dt_in, double T_in, double xc_in, double simgax_in, double px_in, double yx_in, double sigmay_in, double py_in, int v0_in);
 
     // Tranlates a pair of indices (i,j) into a corresponding single index k
     int translate_indices(int i, int j); 
@@ -27,10 +52,10 @@ class DoubleSlitBox
     // Evolves the system one time step (dt) using the Crank-Nicholson scheme
     void evolve_CN(double dt);
     
-    // Sets up initial state based on an unnormalised Gaussian wave packet epression
-    void init_box();
+    // Sets up initial state vector u0 based on an unnormalised Gaussian wave packet epression
+    void init_wave();
 
-    // initializes the potential V
+    // initializes the potential V matrix
     void init_V();
 
 };
